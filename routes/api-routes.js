900fc4b8968890b3c1306a4f9864673f38979dcc
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-12-17 10:00:13 -0700

    Working on Routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,12 +14,14 @@ module.exports = function (app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", function (req, res) {
-    db.User.create({
-      first_name: req.body.first,
-      last_name: req.body.last,
-      email: req.body.email,
-      password: req.body.password,
-    })
+    console.log(req.body);
+    db.user
+      .create({
+        first_name: req.body.first,
+        last_name: req.body.last,
+        email: req.body.email,
+        password: req.body.password,
+      })
       .then(function () {
         res.redirect(307, "/api/login");
       })
@@ -35,33 +37,41 @@ module.exports = function (app) {
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function (req, res) {
+  app.get("/api/user_data", async function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
     } else {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id,
-      });
+      try {
+        // return the result to the user with res.json
+        res.json(
+          await db.List.findAll({
+            where: {
+              user_id: req.user.id,
+            },
+          })
+        );
+      } catch (err) {
+        res.status(500).end();
+      }
     }
   });
 
-  app.get("/api/user_data", async function (req, res) {
-    // Add sequelize code to find all posts where the category is equal to req.params.category,
-    try {
-      // return the result to the user with res.json
-      res.json(
-        await db.List.findAll({
-          where: {
-            user_id: req.user.id,
-          },
-        })
-      );
-    } catch (err) {
-      res.status(500).end();
-    }
-  });
+  // app.get("/api/user_data", async function (req, res) {
+  //   // Add sequelize code to find all posts where the category is equal to req.params.category,
+  //   try {
+  //     // return the result to the user with res.json
+  //     res.json(
+  //       await db.List.findAll({
+  //         where: {
+  //           user_id: req.user.id,
+  //         },
+  //       })
+  //     );
+  //   } catch (err) {
+  //     res.status(500).end();
+  //   }
+  // });
 };
