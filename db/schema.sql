e149fba5649294fa79e064107cbc58dd1ac6b90c
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-12-16 22:06:11 -0700

    Schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,35 +4,28 @@ CREATE DATABASE `jingle_all_the_way_db`;
 USE jingle_all_the_way_db;
 
 CREATE TABLE `list` (
-  `listid` INT(50) NOT NULL AUTO_INCREMENT,
+  `id` INT(50) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`listid`)
+  `user_id` VARCHAR(50) NOT NULL REFERENCES user.id,
+  PRIMARY KEY (`id`)
 );
-
-INSERT INTO `list` (`listid`, `name`)
-VALUES (1, 'test');
 
 
 CREATE TABLE `user` (
   `id` INT(50) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(50) NOT NULL,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `user` (`id`, `username`, `password`, `email`)
-VALUES (1, 'test', 'test', 'test@test.com');
-
-
-
-
 CREATE TABLE `gift_table` (
-  `listid` INT(50) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50) NOT NULL,
-  `gift` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`listid`)
+  `id` INT(50) NOT NULL AUTO_INCREMENT,
+  `list_id` INT(50) NOT NULL REFERENCES list.id,
+  `gift_name` VARCHAR(50) NOT NULL,
+  `gift_url` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+  
 );
 
-INSERT INTO `gift_table` (`listid`, `name`, `gift`)
-VALUES (1, 'test', 'test');
