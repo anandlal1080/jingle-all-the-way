4461ccaca060aa2acadffbaa6938335400205c51
Xis <rick-s@hotmail.com>
2020-12-17 11:23:20 -0800

    working on second API call for images

--- public/js/script.js
+++ public/js/script.js
@@ -3,22 +3,42 @@ const { window } = new JSDOM("");
 const $ = require("jquery")(window);
 
 const cors = "https://cors-anywhere.herokuapp.com/";
-
+const key = "8dmo8ta4dscian61zluqduys";
+let justThePic = [];
 initialApiCall();
 
 function initialApiCall() {
   $.ajax({
-    url:
-      cors +
-      "https://openapi.etsy.com/v2/listings/active?api_key=8dmo8ta4dscian61zluqduys",
+    url: cors + "https://openapi.etsy.com/v2/listings/active/?api_key=" + key,
     type: "GET",
   }).then(function (data) {
     const allResults = data.results;
-    console.log(allResults.length);
-
+    const listingIdNums = [];
     for (let index = 0; index < allResults.length; index++) {
-        const resultItem = allResults[index];
-        
+      const resultItem = allResults[index];
+      listingIdNums.push(resultItem.listing_id);
     }
+    secondApiCall(listingIdNums);
   });
 }
+function secondApiCall(listingIds) {
+  for (let index = 0; index < listingIds.length; index++) {
+    const singleId = listingIds[index];
+    $.ajax({
+      url:
+        "https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v2/listings/" +
+        singleId +
+        "/images?api_key=" +
+        key,
+      type: "GET",
+    }).then(function (data) {
+      const allImages = data.results;
+
+      for (let index = 0; index < allImages.length; index++) {
+        justThePic.push(allImages[index].url_170x135);
+      }
+    });
+  }
+}
+// API call using the listing ID
+// https://openapi.etsy.com/v2/listings/864793648/images?api_key=8dmo8ta4dscian61zluqduys
